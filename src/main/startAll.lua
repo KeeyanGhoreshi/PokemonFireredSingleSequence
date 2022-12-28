3384e81ff2e49b45582fb24263fff598cf1e2639
Frumpy9 <32330391+Frumpy9@users.noreply.github.com>
2022-12-28 02:18:37 -0500

    rename, fix, and set back to 1

---
+++ src/main/startAll.lua
@@ -0,0 +1,8 @@
+console.clear()
+package.path = "./main/?.lua;"
+print("Starting Pokemon Fire Red inputs.")
+for i = 1, 9 do
+    local filename = string.format("%d.lua", i)
+    local chunk = assert(loadfile(filename))
+    chunk()
+end
